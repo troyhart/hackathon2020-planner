96261ff5678147afc3442d8624634558b9b199c3
Troy Hart <thart@myriad.com>
2020-03-20 16:57:52 -0600

    fixed path issue

--- src/main/java/com/example/planner/DemoApplication.java
+++ src/main/java/com/example/planner/DemoApplication.java
@@ -1,21 +1,16 @@
 package com.example.planner;
 
-import java.util.Collections;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 @RestController
@@ -38,7 +33,7 @@ public class DemoApplication {
 	public List<String> getMethodName(@PathVariable("planName") String planName) {
 
 		RestTemplate restTemplate = new RestTemplate();
-		List list = restTemplate.getForObject(planSetUri+"/"+planName, List.class);
+		List list = restTemplate.getForObject(planSetUri+"/plan-sets/"+planName, List.class);
 		return list;
 	}
 }
